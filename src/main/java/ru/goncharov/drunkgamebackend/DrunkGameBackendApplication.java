48e0c1d5b3d14c8bc8a8251ab3d1e3783daf27b0
mgoncharov <mgoncharov@vp.ru>
2020-09-03 19:17:15 +0600

    first commit

---
+++ src/main/java/ru/goncharov/drunkgamebackend/DrunkGameBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.goncharov.drunkgamebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrunkGameBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrunkGameBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/goncharov/drunkgamebackend/DrunkGameBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.goncharov.drunkgamebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrunkGameBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
